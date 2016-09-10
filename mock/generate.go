d2e5b80661eeff22061f31ca9b773ca8e157e962
David Chung <david.chung@docker.com>
2016-09-09 22:42:48 -0700

    test mocks

---
+++ mock/generate.go
@@ -0,0 +1,4 @@
+package mock
+
+//go:generate mockgen -package mock -destination ec2/mock_ec2iface.go github.com/aws/aws-sdk-go/service/ec2/ec2iface EC2API
+//go:generate mockgen -package client -destination docker/engine-api/client/api.go github.com/docker/engine-api/client APIClient
