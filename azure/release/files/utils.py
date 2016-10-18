361aede54b9a17f2f52d1784a6588089f3549b2d
French Ben <frenchben@docker.com>
2016-10-17 17:45:12 -0700

    Removed ddc dev and added variables for cloud
    Signed-off-by: French Ben <frenchben@docker.com>

--- azure/release/files/utils.py
+++ azure/release/files/utils.py
@@ -135,19 +135,28 @@ def create_rg_cloud_template(release_channel, docker_version,
                 }
             },
             "DockerCloudUsername" : {
-                "type" : "String",
+                "type" : "string",
                 "metadata": {
                     "description": "Docker ID username to use during registration\n\nMust only contain letters or digits"
                 }
             },
             "DockerCloudAPIKey" : {
-                "type" : "String",
+                "type" : "securestring",
                 "metadata": {
                     "description": "Docker ID API key to use during registration"
                 }
             }
         }
         parameters.update(new_parameters)
+    
+    variables = data.get('variables')
+    if variables:
+        new_variables = {
+            "dockerCloudClusterName" : "[parameters('DockerCloudClusterName')]",
+            "dockerCloudUsername" : "[parameters('DockerCloudUsername')]",
+            "dockerCloudAPIKey" : "[parameters('DockerCloudAPIKey')]",
+        }
+        variables.update(new_variables)
 
     outputs = data.get('outputs')
     if outputs:
@@ -198,147 +207,3 @@ def create_rg_cloud_template(release_channel, docker_version,
       print(u"ERROR: Cloudformation template invalid in {}".format(outfile))
 
     return upload_rg_template(release_channel, cloudformation_template_name, outfile)
-
-
-def create_ddc_dev_cfn_template(amis, release_channel, docker_version,
-                                docker_for_aws_version, edition_version, cfn_template, cfn_name):
-    """This will create the ddc dev template, which is for internal use only."""
-    # check if file exists before opening.
-
-    flat_edition_version = edition_version.replace(" ", "").replace("_", "").replace("-", "")
-    flat_edition_version_upper = flat_edition_version.capitalize()
-
-    with open(cfn_template) as data_file:
-        data = json.load(data_file)
-
-    parameters = data.get('Parameters')
-    if parameters:
-        new_parameters = {
-            "HubNamespaceSet": {
-                "Type": "String",
-                "Description": "Hub namespace to pull Datacenter?",
-                "AllowedValues": ["docker", "dockerorcadev"],
-                "Default": "dockerorcadev"
-            },
-            "HubTagSet": {
-                "Type": "String",
-                "Description": "Hub tag to pull Datacenter?",
-                "ConstraintDescription": "Please enter the image tag you want to use for pulling Docker Datacenter",
-                "Default": "2.0.0-beta1"
-            },
-            "DockerIdSet": {
-                "Type": "String",
-                "Description": "Docker ID for pulling Datacenter image?",
-                "ConstraintDescription": "A Docker account that has access to the specified Datacenter image on Docker Hub"
-            },
-            "DockerIdPasswordSet": {
-                "Type": "String",
-                "Description": "Docker password pulling image?",
-                "ConstraintDescription": "Docker password corresponding to the Docker ID",
-                "NoEcho": "true"
-            }
-        }
-        parameters.update(new_parameters)
-
-    outputs = data.get('Outputs')
-    if outputs:
-        new_outputs = {
-            "TheHubNamespace": {
-                "Description": "Docker Hub namespace",
-                "Value": {
-                    "Ref": "HubNamespaceSet"
-                }
-            },
-            "TheHubTag": {
-                "Description": "Docker Hub tag",
-                "Value": {
-                    "Ref": "HubTagSet"
-                }
-            },
-            "TheDockerId": {
-                "Description": "Docker ID",
-                "Value": {
-                    "Ref": "DockerIdSet"
-                }
-            },
-        }
-        outputs.update(new_outputs)
-
-    metadata = data.get('Metadata')
-    if metadata:
-        new_optional_parameters = ["HubNamespaceSet", "HubTagSet", "DockerIdSet", "DockerIdPasswordSet"]
-        new_parameter_labels = {
-            "DockerIdSet": {"default": "Docker ID for installing Docker Datacenter?"},
-            "DockerIdPasswordSet": {"default": "docker ID Password for installing Docker Datacenter?"},
-            "HubNamespaceSet": {"default": "Hub namespace for installing Docker Datacenter?"},
-            "HubTagSet": {"default": "Hub tag for installing Docker Datacenter?"},
-        }
-        try:
-            for group in metadata["AWS::CloudFormation::Interface"]["ParameterGroups"]:
-                if group["Label"]["default"] == "Optional Features":
-                    group["Parameters"] += new_optional_parameters
-            metadata["AWS::CloudFormation::Interface"]['ParameterLabels'].update(new_parameter_labels)
-        except ValueError as e:
-            print("Updating metadata failed: {}", e.args)
-
-    data['Description'] = u"Docker for AWS {0} ({1}) DEV".format(docker_version, edition_version)
-    data['Mappings']['DockerForAWS'] = {u'version': {u'docker': docker_version,
-                                                     u'forAws': docker_for_aws_version}}
-    data['Mappings']['AWSRegionArch2AMI'] = amis
-
-    manager_launch_config_orig_key = 'ManagerLaunchConfig'
-    manager_launch_config_new_key = u"{}{}".format(
-        manager_launch_config_orig_key, flat_edition_version_upper)
-    node_launch_config_orig_key = 'NodeLaunchConfig'
-    node_launch_config_new_key = u"{}{}".format(
-        node_launch_config_orig_key, flat_edition_version_upper)
-
-    # loop through the resouces finding the correct Config node names.
-    for key in data.get('Resources'):
-        if key.startswith(manager_launch_config_orig_key):
-            # we found the full name, change variable to current key value.
-            manager_launch_config_orig_key = key
-        elif key.startswith(node_launch_config_orig_key):
-            # we found the full name, change variable to current key value.
-            node_launch_config_orig_key = key
-
-    docker_launch_command = 'docker4x/ddc-init-aws:$DOCKER_FOR_IAAS_VERSION\n'
-    new_content = ["-e HUB_NAMESPACE='", {"Ref": "HubNamespaceSet"}, "' ",
-                   "-e HUB_TAG='", {"Ref": "HubTagSet"}, "' ",
-                   "-e DOCKER_ID='", {"Ref": "DockerIdSet"}, "' ",
-                   "-e DOCKER_ID_PASSWORD='", {"Ref": "DockerIdPasswordSet"}, "' ",
-                  ]
-
-    data['Resources'][manager_launch_config_new_key] = data['Resources'].pop(
-        manager_launch_config_orig_key)
-    data['Resources']['ManagerAsg']['Properties']['LaunchConfigurationName']['Ref'] = manager_launch_config_new_key
-
-    cmd_array = data['Resources'][manager_launch_config_new_key]['Properties']['UserData']['Fn::Base64']['Fn::Join'][1]
-    insert_location = cmd_array.index(docker_launch_command)
-    cmd_array = cmd_array[:insert_location] + new_content + cmd_array[insert_location:]
-    data['Resources'][manager_launch_config_new_key]['Properties']['UserData']['Fn::Base64']['Fn::Join'][1] = cmd_array
-
-    data['Resources'][node_launch_config_new_key] = data['Resources'].pop(
-        node_launch_config_orig_key)
-    data['Resources']['NodeAsg']['Properties']['LaunchConfigurationName']['Ref'] = node_launch_config_new_key
-
-    cmd_array_node = data['Resources'][node_launch_config_new_key]['Properties']['UserData']['Fn::Base64']['Fn::Join'][1]
-    insert_location_node = cmd_array_node.index(docker_launch_command)
-    cmd_array_node = cmd_array_node[:insert_location_node] + new_content + cmd_array_node[insert_location_node:]
-    data['Resources'][node_launch_config_new_key]['Properties']['UserData']['Fn::Base64']['Fn::Join'][1] = cmd_array_node
-
-    cloudformation_template_name = u"{}.json".format(cfn_name)
-    outdir = u"dist/azure/{}".format(release_channel)
-    # if the directory doesn't exist, create it.
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
-
-    outfile = u"{}/{}".format(outdir, cloudformation_template_name)
-
-    with open(outfile, 'w') as outf:
-        json.dump(data, outf, indent=4, sort_keys=True)
-
-    # TODO: json validate
-
-    print(u"DDC Dev Cloudformation template created in {}".format(outfile))
-    return upload_cfn_template(release_channel, cloudformation_template_name, outfile, cfn_type="dev")
