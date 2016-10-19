b8949ac242f7e62b53c18c1c10be98d303a31a10
French Ben <frenchben@docker.com>
2016-10-19 13:47:56 -0700

    Added changes made by deep
    Signed-off-by: French Ben <frenchben@docker.com>

--- azure/release/files/custom-data_manager_cloud.sh
+++ azure/release/files/custom-data_manager_cloud.sh
@@ -7,12 +7,15 @@ export GROUP_NAME="variables('groupName')"
 export LB_NAME="variables('lbName')"
 export APP_ID="variables('adServicePrincipalAppID')"
 export APP_SECRET="variables('adServicePrincipalAppSecret')"
+export TENANT_ID="variables('tenantID')"
 export CLOUD_USER="variables('dockerCloudUsername')"
 export CLOUD_KEY="variables('dockerCloudAPIKey')"
 export SWARM_NAME="variables('dockerCloudClusterName')"
+export SWARM_INFO_TABLE="variables('swarmInfoTable')"
+export SWARM_INFO_STORAGE_ACCOUNT="variables('swarmInfoStorageAccount')"
  
 
-docker run --restart=no -d -e ROLE="$ROLE" -e REGION="$REGION" -e ACCOUNT_ID="$ACCOUNT_ID" -e PRIVATE_IP="$MANAGER_IP" -e DOCKER_FOR_IAAS_VERSION="$DOCKER_FOR_IAAS_VERSION" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /var/log:/var/log docker4x/init-azure:"$DOCKER_FOR_IAAS_VERSION"
+docker run --restart=no -d -e ROLE="$ROLE" -e REGION="$REGION" -e TENANT_ID="$TENANT_ID" -e APP_ID="$APP_ID" -e APP_SECRET="$APP_SECRET" -e ACCOUNT_ID="$ACCOUNT_ID" -e PRIVATE_IP="$MANAGER_IP" -e DOCKER_FOR_IAAS_VERSION="$DOCKER_FOR_IAAS_VERSION" -e SWARM_INFO_TABLE="$SWARM_INFO_TABLE" -e SWARM_INFO_STORAGE_ACCOUNT="$SWARM_INFO_STORAGE_ACCOUNT" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /var/log:/var/log docker4x/init-azure:"$DOCKER_FOR_IAAS_VERSION"
 docker run --restart=always -d -e ROLE="$ROLE -e REGION="$REGION -e ACCOUNT_ID="$ACCOUNT_ID" -e PRIVATE_IP="$MANAGER_IP" -e DOCKER_FOR_IAAS_VERSION="$DOCKER_FOR_IAAS_VERSION" -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker docker4x/guide-azure:"$DOCKER_FOR_IAAS_VERSION"
 echo default: "$LB_NAME" >> /var/lib/docker/swarm/elb.config
 echo "$LB_NAME" > /var/lib/docker/swarm/lb_name
