a2aed7cf7a0fb40a0e83b00655066062541d3a36
Ken Cochrane <KenCochrane@gmail.com>
2016-11-11 16:32:55 -0500

    clean up after build

--- tools/metaserver/build.sh
+++ tools/metaserver/build.sh
@@ -6,7 +6,8 @@ DOCKER_IMAGE_NAME="metaserver"
 DOCKER_CONTAINER_NAME="metaserver-build-container"
 
 if [[ $(docker ps -a | grep $DOCKER_CONTAINER_NAME) != "" ]]; then
-  docker rm -f $DOCKER_CONTAINER_NAME
+    echo "remove $DOCKER_CONTAINER_NAME"
+    docker rm -f $DOCKER_CONTAINER_NAME
 fi
 
 docker build -t $DOCKER_IMAGE_NAME .
@@ -20,3 +21,8 @@ mkdir -p ../../aws/dockerfiles/files/bin/ ../../azure/dockerfiles/files/bin/
 rm -f ../../aws/dockerfiles/files/bin/metaserver ../../azure/dockerfiles/files/bin/metaserver
 cp bin/metaserver ../../aws/dockerfiles/files/bin/metaserver
 cp bin/metaserver ../../azure/dockerfiles/files/bin/metaserver
+
+if [[ $(docker ps -a | grep $DOCKER_CONTAINER_NAME) != "" ]]; then
+    echo "remove $DOCKER_CONTAINER_NAME"
+    docker rm -f $DOCKER_CONTAINER_NAME
+fi
