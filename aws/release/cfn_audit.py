d8902e3d1c4a274d3b301a178fa82c2655a50ccf
Ken Cochrane <KenCochrane@gmail.com>
2016-12-15 14:48:44 -0500

    Added new AWS london region
    Signed-off-by: Ken Cochrane <KenCochrane@gmail.com>

--- aws/release/cfn_audit.py
+++ aws/release/cfn_audit.py
@@ -7,7 +7,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 REGIONS = ['us-west-1', 'us-west-2', 'us-east-1',
-           'eu-west-1', 'eu-central-1', 'ap-southeast-1',
+           'eu-west-1', 'eu-west-2', 'eu-central-1', 'ap-southeast-1',
            'ap-northeast-1', 'ap-southeast-2', 'ap-northeast-2',
            'sa-east-1', 'ap-south-1', 'us-east-2', 'ca-central-1']
 
