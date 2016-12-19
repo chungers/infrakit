5478778aa598ca370aa20f91f10f018ccaff1b2a
Deep Debroy <ddebroy@docker.com>
2016-12-19 14:45:23 -0800

    fix typo

--- azure/dockerfiles/files/guide/refresh.sh
+++ azure/dockerfiles/files/guide/refresh.sh
@@ -14,7 +14,7 @@ if [[ "$IS_LEADER" == "true" ]]; then
     echo "Current manager IP = $CURRENT_MANAGER_IP ; my IP = $MYIP"
 
     if [ "$CURRENT_MANAGER_IP" != "$MYIP" ]; then
-        echo "Swarm Manager IP cwqhanged, updating azure table with new ip"
+        echo "Swarm Manager IP changed, updating azure table with new ip"
         python /usr/bin/azureleader.py insert-ip $MYIP
     fi
 
