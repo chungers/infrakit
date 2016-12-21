a4746fccbf3ae533bb30bf84a3f08feb53b0c7e2
Deep Debroy <ddebroy@docker.com>
2016-12-21 03:06:02 -0800

    fix IP determination in az refresh

--- azure/dockerfiles/files/guide/refresh.sh
+++ azure/dockerfiles/files/guide/refresh.sh
@@ -9,7 +9,7 @@ IS_LEADER=$(docker node inspect self -f '{{ .ManagerStatus.Leader }}')
 
 if [[ "$IS_LEADER" == "true" ]]; then
     # we are the leader, We only need to call once, so we only call from the current leader.
-    MYIP=$(ifconfig eth0 | grep "inet addr:" | cut -d: -f2 | cut -d" " -f1)
+    MYIP=$PRIVATE_IP
     CURRENT_MANAGER_IP=$(python /usr/bin/azureleader.py get-ip)
     echo "Current manager IP = $CURRENT_MANAGER_IP ; my IP = $MYIP"
 
