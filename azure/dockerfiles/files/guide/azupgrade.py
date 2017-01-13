99fa5f04812123ba76fb7e2c945ccb2659780c96
Deep Debroy <ddebroy@docker.com>
2017-01-12 23:51:15 -0800

    Improve logging for long running azure scripts and daemons

--- azure/dockerfiles/files/guide/azupgrade.py
+++ azure/dockerfiles/files/guide/azupgrade.py
@@ -7,6 +7,8 @@ import sys
 import subprocess
 import pytz
 import urllib2
+import logging
+import logging.config
 from datetime import datetime
 from time import sleep
 from docker import Client
@@ -21,6 +23,9 @@ from azure.mgmt.storage.models import StorageAccountCreateParameters
 from azure.storage.table import TableService, Entity
 from azure.storage.queue import QueueService
 from azutils import *
+
+LOG_CFG_FILE = "/etc/azupgrade_log_cfg.json"
+LOG = logging.getLogger("azupg")
 
 APP_SECRET_PARAMETER_NAME_IN_TEMPLATE = 'adServicePrincipalAppSecret'
 
@@ -43,7 +48,7 @@ def validate_template(template_url):
 
 def update_deployment_template(template_url, resource_client):
 
-    print("Updating Resource Group: {}".format(RG_NAME))
+    LOG.info("Updating Resource Group: {}".format(RG_NAME))
 
     deployments = resource_client.deployments.list(RG_NAME)
     latest_timestamp = datetime.min
@@ -55,7 +60,7 @@ def update_deployment_template(template_url, resource_client):
 
     for deployment in deployments:
         state = deployment.properties.provisioning_state
-        print(("Inspecting deployment: {} at state {}").format(
+        LOG.info(("Inspecting deployment: {} at state {}").format(
                 deployment.name, state))
         if state != "Succeeded":
             continue
@@ -66,10 +71,10 @@ def update_deployment_template(template_url, resource_client):
             latest_deployment = deployment
 
     if latest_deployment is None:
-        print("No successful deployment found for {}".format(RG_NAME))
+        LOG.error("No successful deployment found for {}".format(RG_NAME))
         return
 
-    print("Found deployment: {} deployed at {}".format(
+    LOG.info("Found deployment: {} deployed at {}".format(
             latest_deployment.name, latest_deployment.properties.timestamp))
 
     # update link to latest template.json
@@ -83,21 +88,21 @@ def update_deployment_template(template_url, resource_client):
     }
 
     if latest_deployment.properties.parameters is None:
-        print("ERROR: Upgrading Deployment with Parameter Link Not Supported")
-        print("Parameters need to be specified as part of the deployment")
+        LOG.error("Upgrading Deployment with Parameter Link Not Supported")
+        LOG.error("Parameters need to be specified as part of the deployment")
         return
 
     deployment_properties['parameters'] = {}
-    print("Using Parameters Object from previous deployment {}".format(
+    LOG.info("Using Parameters Object from previous deployment {}".format(
             latest_deployment.properties.parameters))
     # azure requires the type field to be removed from the JSON
     # certain fields like secure strings won't have a value
     for k, v in latest_deployment.properties.parameters.items():
         if 'value' in v:
-            print("Adding parameter: {} value: {}".format(k, v['value']))
+            LOG.info("Adding parameter: {} value: {}".format(k, v['value']))
             deployment_properties['parameters'][k] = {'value': v['value']}
         else:
-            print("Skipping parameter: {} contents: {}".format(k, v))
+            LOG.info("Skipping parameter: {} contents: {}".format(k, v))
 
     # secure strings need to be populated separately as they are not returned
     # APP_SECRET is the only secure string parameter user populates now
@@ -108,7 +113,7 @@ def update_deployment_template(template_url, resource_client):
                         RG_NAME, latest_deployment.name, deployment_properties)
     wait_with_status(async_update, "Waiting for deployment to update ...")
 
-    print("Finished updating deployment: {}".format(latest_deployment.name))
+    LOG.info("Finished updating deployment: {}".format(latest_deployment.name))
 
 
 def update_vmss(vmss_name, docker_client, compute_client, network_client, tbl_svc):
@@ -121,28 +126,27 @@ def update_vmss(vmss_name, docker_client, compute_client, network_client, tbl_sv
     nics = network_client.network_interfaces.list_virtual_machine_scale_set_network_interfaces(
                                                             RG_NAME, vmss_name)
     for nic in nics:
-        # print ("NIC: {} Primary:{}").format(nic.id, nic.primary)
+        LOG.debug("NIC: {} Primary:{}".format(nic.id, nic.primary))
         if nic.primary:
             for ips in nic.ip_configurations:
-                # print ("IP: {} Primary:{}").format(ips.private_ip_address, ips.primary)
+                LOG.debug("IP: {} Primary:{}".format(ips.private_ip_address, ips.primary))
                 if ips.primary:
                     nic_id_table[nic.id] = ips.private_ip_address
 
     vms = compute_client.virtual_machine_scale_set_vms.list(RG_NAME, vmss_name)
     for vm in vms:
-        print("Getting IP of VM: {} in VMSS {}".format(vm.instance_id, vmss_name))
+        LOG.debug("Enumerate NICs of VM: {} in VMSS {}".format(vm.instance_id, vmss_name))
         for nic in vm.network_profile.network_interfaces:
             if nic.id in nic_id_table:
-                # print ("IP Address: {}").format(nic_ip_table[nic.id])
+                LOG.debug("IP Address of NIC: {}".format(nic_id_table[nic.id]))
                 vm_ip_table[nic_id_table[nic.id]] = vm.instance_id
 
     nodes = docker_client.nodes(filters={'role': VMSS_ROLE_MAPPING[vmss_name]})
     for node in nodes:
         node_ip = node['Status']['Addr']
-        print("Node ID: {} IP: {}".format(node['ID'], node_ip))
+        LOG.debug("Swarm Node ID: {} IP: {}".format(node['ID'], node_ip))
         if node_ip not in vm_ip_table:
-            print("ERROR: Node IP {} not found in list of VM IPs {}".format(
-                    node_ip, vm_ip_table))
+            LOG.error("Node IP {} not found in list of VM IPs".format(node_ip))
             return
         vm_id_table[vm_ip_table[node_ip]] = node['ID']
 
@@ -150,11 +154,11 @@ def update_vmss(vmss_name, docker_client, compute_client, network_client, tbl_sv
     # beyond this point it's hard to recover from any missing data/errors
     for vm in vms:
         node_id = vm_id_table[vm.instance_id]
-        print("Accessing VM: {} in VMSS {}".format(vm.instance_id, vmss_name))
+        LOG.info("Accessing VM: {} in VMSS {}".format(vm.instance_id, vmss_name))
 
         # skip the node we are invoking from for now
         if docker_client.info()['Swarm']['NodeID'] == node_id:
-            print("Skip upgrading of manager node where upgrade is initiated from for now")
+            LOG.info("Skip upgrading of manager node where upgrade is initiated from for now")
             continue
 
         # demote managers and keep track of new leader
@@ -172,29 +176,29 @@ def update_vmss(vmss_name, docker_client, compute_client, network_client, tbl_sv
             # If node was a leader, update IP of new leader
             # (after demotion of previous leader) in table
             if leader:
-                print("Previous Leader demoted. Update leader IP address")
+                LOG.info("Previous Leader demoted. Update leader IP address")
                 leader_ip = get_swarm_leader_ip(docker_client)
                 update_leader_tbl(tbl_svc, SWARM_TABLE, LEADER_PARTITION,
                                         LEADER_ROW, leader_ip)
 
         subprocess.check_output(["docker", "node", "rm", "--force", node_id])
 
-        print("Update OS info started for VMSS node: {}".format(vm.instance_id))
+        LOG.info("Update OS info started for VMSS node: {}".format(vm.instance_id))
         async_vmss_update = compute_client.virtual_machine_scale_sets.update_instances(
                                             RG_NAME, vmss_name, vm.instance_id)
         wait_with_status(async_vmss_update, "Waiting for VM OS info update to complete ...")
-        print("Update OS info completed for VMSS node: {}".format(vm.instance_id))
+        LOG.info("Update OS info completed for VMSS node: {}".format(vm.instance_id))
 
-        print "Reimage started for VMSS node: {}".format(vm.instance_id)
+        LOG.info("Reimage started for VMSS node: {}".format(vm.instance_id))
         async_vmss_update = compute_client.virtual_machine_scale_set_vms.reimage(
                                             RG_NAME, vmss_name, vm.instance_id)
         wait_with_status(async_vmss_update, "Waiting for VM reimage to complete ...")
-        print("Reimage completed for VMSS node: {}".format(vm.instance_id))
+        LOG.info("Reimage completed for VMSS node: {}".format(vm.instance_id))
 
         node_booting = True
         while node_booting:
             sleep(10)
-            print("Waiting for VMSS node:{} to boot and join back in swarm".format(
+            LOG.info("Waiting for VMSS node:{} to boot and join back in swarm".format(
                     vm.instance_id))
             for node in docker_client.nodes():
                 try:
@@ -204,13 +208,17 @@ def update_vmss(vmss_name, docker_client, compute_client, network_client, tbl_sv
                 except KeyError:
                     # When a member is joining, sometimes things
                     # are a bit unstable and keys are missing. So retry.
-                    print("Description/Hostname not found. Retrying ..")
+                    LOG.info("Description/Hostname not found. Retrying ..")
                     continue
-        print("VMSS node:{} successfully connected back to swarm".format(
+        LOG.info("VMSS node:{} successfully connected back to swarm".format(
                 vm.instance_id))
 
 
 def main():
+
+    with open(LOG_CFG_FILE) as log_cfg_file:
+        log_cfg = json.load(log_cfg_file)
+        logging.config.dictConfig(log_cfg)
 
     parser = argparse.ArgumentParser(description='Upgrade a Docker4Azure resource group')
     parser.add_argument('template_url', help='New template to upgrade to')
@@ -238,48 +246,48 @@ def main():
     tbl_svc = TableService(account_name=SA_NAME, account_key=storage_keys['key1'])
 
     try:
-        print("Validate Template URL to upgrade to")
+        LOG.info("Validate Template URL to upgrade to")
         validate_template(args.template_url)
     except:
-        print("ERROR: Template validation failed. Please make sure the template URL has a valid JSON file and is accessible.")
+        LOG.error("Template validation failed. Please make sure the template URL has a valid JSON file and is accessible.")
         raise
 
     qsvc = QueueService(account_name=SA_NAME, account_key=storage_keys['key1'])
     # the Upgrade Msg Queue should only exist when an upgrade is in progress
     if qsvc.exists(UPGRADE_MSG_QUEUE):
-        print "Upgrade message queue already exists. Please make sure another upgrade is not in progress."
+        LOG.error("Upgrade message queue already exists. Please make sure another upgrade is not in progress.")
         return
 
-    print("Initiating upgrade. Create queue to prevent another simultaneous upgrade.")
+    LOG.info("Initiating upgrade. Create queue to prevent another simultaneous upgrade.")
     qsvc.create_queue(UPGRADE_MSG_QUEUE)
 
     try:
         # Update the resource group template
-        print("Updating Resource Group template. This will take several minutes. You can follow the status of the upgrade below or from the Azure console using the URL below:")
-        print("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/overview".format(
+        LOG.info("Updating Resource Group template. This will take several minutes. You can follow the status of the upgrade below or from the Azure console using the URL below:")
+        LOG.info("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/overview".format(
                 SUB_ID, RG_NAME))
         update_deployment_template(args.template_url, resource_client)
 
         # Update manager nodes (except the one this script is initiated from)
-        print("Starting rolling upgrade of swarm manager nodes. This will take several minutes. You can follow the status of the upgrade below or from the Azure console using the URL below:")
-        print("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Compute/virtualMachineScaleSets/{}/overview".format(
+        LOG.info("Starting rolling upgrade of swarm manager nodes. This will take several minutes. You can follow the status of the upgrade below or from the Azure console using the URL below:")
+        LOG.info("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Compute/virtualMachineScaleSets/{}/overview".format(
                 SUB_ID, RG_NAME, MGR_VMSS_NAME))
         update_vmss(MGR_VMSS_NAME, docker_client, compute_client, network_client, tbl_svc)
 
         # Update worker nodes
-        print("Starting rolling upgrade of swarm worker nodes. This will take several minutes. You can follow the status of the upgrade below or from the Azure console using the URL below:")
-        print("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Compute/virtualMachineScaleSets/{}/overview".format(
+        LOG.info("Starting rolling upgrade of swarm worker nodes. This will take several minutes. You can follow the status of the upgrade below or from the Azure console using the URL below:")
+        LOG.info("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Compute/virtualMachineScaleSets/{}/overview".format(
                 SUB_ID, RG_NAME, WRK_VMSS_NAME))
         update_vmss(WRK_VMSS_NAME, docker_client, compute_client, network_client, tbl_svc)
 
-        print("The current VM will be rebooted soon for an upgrade. You can follow the status of the upgrade from the Azure console using the URL below:")
-        print("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Compute/virtualMachineScaleSets/{}/overview".format(
+        LOG.info("The current VM will be rebooted soon for an upgrade. You can follow the status of the upgrade from the Azure console using the URL below:")
+        LOG.info("https://portal.azure.com/#resource/subscriptions/{}/resourceGroups/{}/providers/Microsoft.Compute/virtualMachineScaleSets/{}/overview".format(
                 SUB_ID, RG_NAME, MGR_VMSS_NAME))
         # Signal another node to upgrade the current node and update leader if necessary
         qsvc.put_message(UPGRADE_MSG_QUEUE, docker_client.info()['Swarm']['NodeID'])
 
     except:
-        print("The upgrade process encountered errors. Details below:")
+        LOG.error("The upgrade process encountered errors:", exc_info=True)
         qsvc.delete_queue(UPGRADE_MSG_QUEUE)
         raise
 
