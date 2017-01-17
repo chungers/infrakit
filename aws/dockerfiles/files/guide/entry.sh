05dfa379caadac5686de972ab41b10ee4a37d998
Deep Debroy <ddebroy@docker.com>
2017-01-17 14:04:24 -0800

    Output AWS guide logs to container log

--- aws/dockerfiles/files/guide/entry.sh
+++ aws/dockerfiles/files/guide/entry.sh
@@ -3,5 +3,11 @@
 # set system wide env variables, so they are available to ssh connections
 /usr/bin/env > /etc/environment
 
+echo "Initialize logging for guide daemons"
+# setup symlink to output logs from relevant scripts to container logs
+ln -s /proc/1/fd/1 /var/log/docker/refresh.log
+ln -s /proc/1/fd/1 /var/log/docker/watcher.log
+ln -s /proc/1/fd/1 /var/log/docker/cleanup.log
+
 # start cron
 /usr/sbin/crond -f -l 9 -L /var/log/cron.log
