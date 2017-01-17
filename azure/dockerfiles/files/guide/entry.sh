76ac0ca9d939af2d448ba6c4ea8366becd49203b
Deep Debroy <ddebroy@docker.com>
2017-01-17 00:54:29 -0800

    Initialize log files and stream to stdout through tail -f

--- azure/dockerfiles/files/guide/entry.sh
+++ azure/dockerfiles/files/guide/entry.sh
@@ -1,7 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 # set system wide env variables, so they are available to ssh connections
 /usr/bin/env > /etc/environment
 
 # start cron
-/usr/sbin/crond -f -l 9 -L /var/log/cron.log
+/usr/sbin/crond -f -l 9 -L /var/log/docker/cron.log &
+
+echo "Initializing logs" > /var/log/docker/buoy.log
+echo "Initializing logs" > /var/log/docker/refresh.log
+echo "Initializing logs" > /var/log/docker/vacuum.log
+echo "Initializing logs" > /var/log/docker/kick_upgrade.log
+echo "Initializing logs" > /var/log/docker/kick_rejoin.log
+echo "Initializing logs" > /var/log/docker/kick_nodewatch.log
+
+echo "Initializing logs" > /var/log/docker/azupg_listener_modules.log
+echo "Initializing logs" > /var/log/docker/azupg_listener_actions.log
+echo "Initializing logs" > /var/log/docker/azupg_listener_daemon.log
+echo "Initializing logs" > /var/log/docker/aznodewatch_modules.log
+echo "Initializing logs" > /var/log/docker/aznodewatch_daemon.log
+echo "Initializing logs" > /var/log/docker/aznodewatch_actions.log
+echo "Initializing logs" > /var/log/docker/azrejoin_modules.log
+echo "Initializing logs" > /var/log/docker/azrejoin_actions.log
+echo "Initializing logs" > /var/log/docker/azrejoin_daemon.log
+echo "Initializing logs" > /var/log/docker/azupgrade_modules.log
+echo "Initializing logs" > /var/log/docker/azupgrade_actions.log
+
+tail -F /var/log/docker/*
--- azure/dockerfiles/files/guide/refresh.sh
+++ azure/dockerfiles/files/guide/refresh.sh
@@ -11,9 +11,9 @@ if [[ "$IS_LEADER" == "true" ]]; then
     # we are the leader, We only need to call once, so we only call from the current leader.
     MYIP=$PRIVATE_IP
     CURRENT_MANAGER_IP=$(python /usr/bin/azureleader.py get-ip)
-    echo "Current manager IP = $CURRENT_MANAGER_IP ; my IP = $MYIP"
 
     if [ "$CURRENT_MANAGER_IP" != "$MYIP" ]; then
+        echo "Current manager IP = $CURRENT_MANAGER_IP ; my IP = $MYIP"
         echo "Swarm Manager IP changed, updating azure table with new ip"
         python /usr/bin/azureleader.py insert-ip $MYIP
     fi
