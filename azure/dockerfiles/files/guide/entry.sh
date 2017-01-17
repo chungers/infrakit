d4ec157e6d748d412f09dad242012b64f1a34253
Deep Debroy <ddebroy@docker.com>
2017-01-17 13:41:48 -0800

    Change logs to direct (1) Action/Info logs to container log and (2) verbose/debug logs to files within guide

--- azure/dockerfiles/files/guide/entry.sh
+++ azure/dockerfiles/files/guide/entry.sh
@@ -3,26 +3,12 @@
 # set system wide env variables, so they are available to ssh connections
 /usr/bin/env > /etc/environment
 
+echo "Initialize logging for guide daemons"
+# setup symlink to output logs from relevant scripts to container logs
+ln -s /proc/1/fd/1 /var/log/docker/refresh.log
+ln -s /proc/1/fd/1 /var/log/docker/azupg_listener_actions.log
+ln -s /proc/1/fd/1 /var/log/docker/aznodewatch_actions.log
+ln -s /proc/1/fd/1 /var/log/docker/azupgrade_actions.log
+
 # start cron
-/usr/sbin/crond -f -l 9 -L /var/log/docker/cron.log &
-
-echo "Initializing logs" > /var/log/docker/buoy.log
-echo "Initializing logs" > /var/log/docker/refresh.log
-echo "Initializing logs" > /var/log/docker/vacuum.log
-echo "Initializing logs" > /var/log/docker/kick_upgrade.log
-echo "Initializing logs" > /var/log/docker/kick_rejoin.log
-echo "Initializing logs" > /var/log/docker/kick_nodewatch.log
-
-echo "Initializing logs" > /var/log/docker/azupg_listener_modules.log
-echo "Initializing logs" > /var/log/docker/azupg_listener_actions.log
-echo "Initializing logs" > /var/log/docker/azupg_listener_daemon.log
-echo "Initializing logs" > /var/log/docker/aznodewatch_modules.log
-echo "Initializing logs" > /var/log/docker/aznodewatch_daemon.log
-echo "Initializing logs" > /var/log/docker/aznodewatch_actions.log
-echo "Initializing logs" > /var/log/docker/azrejoin_modules.log
-echo "Initializing logs" > /var/log/docker/azrejoin_actions.log
-echo "Initializing logs" > /var/log/docker/azrejoin_daemon.log
-echo "Initializing logs" > /var/log/docker/azupgrade_modules.log
-echo "Initializing logs" > /var/log/docker/azupgrade_actions.log
-
-tail -F /var/log/docker/*
+/usr/sbin/crond -f -l 9 -L /var/log/docker/cron.log
