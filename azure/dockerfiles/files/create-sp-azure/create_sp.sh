58932137753de919730efe44949032179afe0cd1
Deep Debroy <ddebroy@docker.com>
2017-01-18 17:04:05 -0800

    enable option to create rg scoped SPs

--- azure/dockerfiles/files/create-sp-azure/create_sp.sh
+++ azure/dockerfiles/files/create-sp-azure/create_sp.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+usage() {
+    echo "usage: ${PROG} app_name [resource_group_name resource_group_location]"
+    echo "    app_name: Name of the new Azure AD Application used for authentication"
+    echo "    resource_group_name: Name of the new Azure Resource Group where s"
+    echo "    location: Location of the new Azure Resource Group"
+}
+
+MAX_RETRIES=20
 PROG="${PROG:-$0}"
 APP_NAME=$1
+RG_NAME=$2
+RG_LOC=$3
 
-if [[ "" == ${APP_NAME} ]]; then
-  echo "Must pass APP_NAME as parameter, i.e. ${PROG} [APP_NAME]."
-  exit 1
+if [ $# -ne 1 ] && [ $# -ne 3 ]; then
+	  usage
+	  exit 1
 fi
 
 azure login
 
 if [ -z "$SUBSCRIPTION_ID" ]; then
-  echo "The following subscriptions were retrieved from your account"
-  PS3='Please select the subscription to use: '
-  options=($(azure account list --json | jq -r 'map(select(.state == "Enabled"))|.[]|.id + ":" + .name' | sed -e 's/ /_/g'))
-  select opt in "${options[@]}"
-  do
-          SUBSCRIPTION_ID=`echo $opt | awk -F ':' '{print $1}'`
-          break
-  done
+    echo "The following subscriptions were retrieved from your account"
+    PS3='Please select the subscription to use: '
+    options=($(azure account list --json | jq -r 'map(select(.state == "Enabled"))|.[]|.id + ":" + .name' | sed -e 's/ /_/g'))
+    select opt in "${options[@]}"
+    do
+        SUBSCRIPTION_ID=`echo $opt | awk -F ':' '{print $1}'`
+        break
+    done
 fi
 
 echo "Using subscription ${SUBSCRIPTION_ID}"
@@ -64,33 +74,94 @@ if [[ "" == ${SP_OBJECT_ID} ]]; then
     exit 1
 fi
 
-echo "Creating role assignment for ${SP_OBJECT_ID} for subscription ${SUBSCRIPTION_ID}"
+if [ ! -z ${RG_NAME} ]; then
+    echo "Create new Azure Resource Group ${RG_NAME} in ${RG_LOC}"
+    azure group create ${RG_NAME} ${RG_LOC}
+    if [[ $? -ne 0 ]]; then
+        echo "Resource Group creation failed. Details:"
+        cat /root/.azure/azure.err
+        exit 1
+    fi
+    echo "Resource Group ${RG_NAME} created "
+fi
 
 echo "Waiting for account updates to complete before proceeding ..."
-sleep 30
-azure role assignment create --objectId ${SP_OBJECT_ID} --roleName Contributor \
-      --scope /subscriptions/${SUBSCRIPTION_ID}/ --json > /var/lib/azure/role_assignment.json
 
-status=$?
-while [[ ${status} != 0 ]];
+sleep 30
+retries=0
+status=1
+while [ ${status} -ne 0 ] && [ ${retries} -lt ${MAX_RETRIES} ];
 do
-    echo "Ignore previous failure and retry ..."
-    sleep 30
-    azure role assignment create --objectId ${SP_OBJECT_ID} --roleName Contributor \
-          --scope /subscriptions/${SUBSCRIPTION_ID}/ --json > /var/lib/azure/role_assignment.json
-    status=$?
+
+    if [ -z ${RG_NAME} ]; then
+        echo "Creating role assignment for ${SP_OBJECT_ID} for subscription ${SUBSCRIPTION_ID}"
+        azure role assignment create --objectId ${SP_OBJECT_ID} --roleName Contributor \
+            --scope /subscriptions/${SUBSCRIPTION_ID}/
+        status=$?
+    else
+        echo "Creating role assignment for ${SP_OBJECT_ID} scoped to ${RG_NAME}"
+        azure role assignment create --objectId ${SP_OBJECT_ID} --roleName Contributor \
+            --resource-group ${RG_NAME}
+        status=$?
+    fi
+
+    if [ ${status} -ne 0 ]; then
+        echo "Details from last failure:"
+        cat /root/.azure/azure.err
+        echo "Wait before retrying ..."
+        sleep 30
+    fi
+    retries=$[retries+1]
 done
 
-echo "Successfully created role assignment for ${SP_OBJECT_ID} for subscription ${SUBSCRIPTION_ID}"
+if [ ${status} -ne 0 ]; then
+    echo "Role assignment creation failed for Azure."
+    echo "we generated the following:"
+    echo "AD ServicePrincipal App ID:       ${APP_ID}"
+    echo "AD ServicePrincipal App Secret:   ${PASSWORD}"
+    exit 1
+fi
+echo "Successfully created role assignment for ${SP_OBJECT_ID}"
 
 echo "Test login..."
-azure login --service-principal --tenant ${TENANT_ID} --username ${APP_ID} --password ${PASSWORD} --json
+echo "Waiting for roles to take effect ..."
+sleep 30
+retires=0
+status=1
+while [ ${status} -ne 0 ] && [ ${retries} -lt ${MAX_RETRIES} ];
+do
+    azure login --service-principal --tenant ${TENANT_ID} --username ${APP_ID} --password ${PASSWORD}
+    status=$?
+
+    if [[ ${status} -ne 0 ]]; then
+        echo "Details from last failure:"
+        cat /root/.azure/azure.err
+        echo "Wait before retrying ..."
+        sleep 30
+    fi
+    retries=$[retries+1]
+done
+
+if [ ${status} -ne 0 ]; then
+    echo "Login failed for Azure with generated SP"
+    echo "we generated the following:"
+    echo "AD ServicePrincipal App ID:       ${APP_ID}"
+    echo "AD ServicePrincipal App Secret:   ${PASSWORD}"
+    exit 1
+fi
 
 echo
-echo 
+echo
 echo
 
 echo "Your access credentials =================================================="
 echo "AD ServicePrincipal App ID:       ${APP_ID}"
 echo "AD ServicePrincipal App Secret:   ${PASSWORD}"
 echo "AD ServicePrincipal Tenant ID:    ${TENANT_ID}"
+if [[ "" == ${RG_NAME} ]]; then
+    echo "WARNING: The above credentials provide full access to your Azure subscription."
+    echo "Please use Service Principals scoped to individual resource groups."
+else
+    echo "Resource Group Name:              ${RG_NAME}"
+    echo "Resource Group Location:          ${RG_LOC}"
+fi
