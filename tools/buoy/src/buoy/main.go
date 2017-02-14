d5d12a6364104bb6757bb055a34e36927f948621
Ken Cochrane <kencochrane@users.noreply.github.com>
2017-02-14 10:30:34 -0500

    Fixed buoy and cleaned up a little (#722)
    Signed-off-by: Ken Cochrane <KenCochrane@gmail.com>

--- tools/buoy/src/buoy/main.go
+++ tools/buoy/src/buoy/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/segmentio/analytics-go"
 )
 
+const NA = "n/a"
+
 func computeHmac256(message string, secret string) string {
 	key := []byte(secret)
 	h := hmac.New(sha256.New, key)
@@ -24,31 +26,31 @@ func main() {
 	numManagers := flag.Int("managers", 0, "Number of Managers")
 	numWorkers := flag.Int("workers", 0, "Number of Workers")
 	numServices := flag.Int("services", 0, "Number of Services")
-	dockerVersion := flag.String("docker_version", "n/a", "Docker Version")
-	iaas_provider := flag.String("iaas_provider", "n/a", "IAAS provider (aws, azure, gcp, etc)")
-	channel := flag.String("channel", "n/a", "IAAS Channel (test, beta, stable, etc)")
-	swarmID := flag.String("swarm_id", "n/a", "Swarm ID")
-	nodeID := flag.String("node_id", "n/a", "Node ID")
-	edition := flag.String("edition", "n/a", "Edition (ce, ee)")
+	dockerVersion := flag.String("docker_version", NA, "Docker Version")
+	iaas_provider := flag.String("iaas_provider", NA, "IAAS provider (aws, azure, gcp, etc)")
+	channel := flag.String("channel", NA, "IAAS Channel (test, beta, stable, etc)")
+	swarmID := flag.String("swarm_id", NA, "Swarm ID")
+	nodeID := flag.String("node_id", NA, "Node ID")
+	edition := flag.String("edition", NA, "Edition (ce, ee)")
 	editionOS := flag.String("editionOS", "moby", "Edition OS (centos, oel, ubuntu, ws2016, rhel, sles)")
-	editionVersion := flag.String("editionVersion", "n/a", "Edition Version")
-	event := flag.String("event", "n/a", "Event") // identify, init, ping, scale
+	editionVersion := flag.String("editionVersion", NA, "Edition Version")
+	event := flag.String("event", NA, "Event") // identify, init, ping, scale
 	dockerForIAASVersion := os.Getenv("DOCKER_FOR_IAAS_VERSION")
 	accountID := os.Getenv("ACCOUNT_ID")
 	region := os.Getenv("REGION")
-
+	edition_env := os.Getenv("EDITION")
 	flag.Parse()
 
-	if *channel == "n/a" {
+	if *channel == NA {
 		channel = getChannel(dockerForIAASVersion)
 	}
 
-	if *editionVersion == "n/a" {
-		editionVersion = dockerForIAASVersion
+	if *editionVersion == NA {
+		editionVersion = &dockerForIAASVersion
 	}
 
-	if *edition == "n/a" {
-		edition = os.Getenv("EDITION")
+	if *edition == NA {
+		edition = &edition_env
 	}
 
 	// Hash the accountId so we don't know what it is.
@@ -62,10 +64,10 @@ func main() {
 		client.Identify(&analytics.Identify{
 			UserId: hashedAccountID,
 			Traits: map[string]interface{}{
-				"region":           region,
-				"edition":          *edition,
-				"edition_os":       *editionOS,
-				"iaas_provider":    *iaas_provider,
+				"region":        region,
+				"edition":       *edition,
+				"edition_os":    *editionOS,
+				"iaas_provider": *iaas_provider,
 			},
 		})
 	} else if *event == "swarm:init" {
@@ -73,11 +75,11 @@ func main() {
 			Event:  "swarm:init",
 			UserId: hashedAccountID,
 			Properties: map[string]interface{}{
-				"swarm_id":         *swarmID,
-				"node_id":          *nodeID,
-				"region":           region,
-				"edition_version":  *editionVersion,
-				"channel":          *channel,
+				"swarm_id":        *swarmID,
+				"node_id":         *nodeID,
+				"region":          region,
+				"edition_version": *editionVersion,
+				"channel":         *channel,
 			},
 		})
 	} else if strings.HasPrefix(*event, "node:") {
@@ -85,11 +87,11 @@ func main() {
 			Event:  *event,
 			UserId: hashedAccountID,
 			Properties: map[string]interface{}{
-				"swarm_id":         *swarmID,
-				"node_id":          *nodeID,
-				"region":           region,
-				"edition_version":  *editionVersion,
-				"channel":          *channel,
+				"swarm_id":        *swarmID,
+				"node_id":         *nodeID,
+				"region":          region,
+				"edition_version": *editionVersion,
+				"channel":         *channel,
 			},
 		})
 	} else if strings.HasPrefix(*event, "swarm:") {
@@ -97,15 +99,15 @@ func main() {
 			Event:  *event,
 			UserId: hashedAccountID,
 			Properties: map[string]interface{}{
-				"swarm_id":                *swarmID,
-				"node_id":                 *nodeID,
-				"region":                  region,
-				"service_count":           *numServices,
-				"manager_count":           *numManagers,
-				"worker_count":            *numWorkers,
-				"docker_version":          *dockerVersion,
-				"edition_version":         *editionVersion,
-				"channel":                 *channel,
+				"swarm_id":        *swarmID,
+				"node_id":         *nodeID,
+				"region":          region,
+				"service_count":   *numServices,
+				"manager_count":   *numManagers,
+				"worker_count":    *numWorkers,
+				"docker_version":  *dockerVersion,
+				"edition_version": *editionVersion,
+				"channel":         *channel,
 			},
 		})
 	}
