3f02ea76a9cff07dfd9120abc8da6913fec8b0bb
David Gageot <david@gageot.net>
2017-03-06 11:59:40 +0100

    Update Moby to 17.03 GA
    Signed-off-by: David Gageot <david@gageot.net>

--- moby/get.sh
+++ moby/get.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-set -e 
+set -e
 
-rm -rf build src packages/aws/dockerimages/
-mkdir -p build/aws build/azure build/gcp src packages/aws/dockerimages/
+mkdir -p build/aws build/azure build/gcp src
 
 docker rm moby || true
-docker create --name moby mobylinux/media:$MOBY_IMG_COMMIT ls
+docker create --name moby mobylinux/media:${MOBY_IMG_COMMIT} true
 docker cp moby:/initrd.img src/initrd.img
 docker cp moby:/vmlinuz64 build/
 docker rm moby
-
-if [ $LOAD_IMAGES == "true" ]; then
-	echo "+ Copying Docker images: cp $ROOTDIR/$AWS_TARGET_PATH/*.tar packages/aws/dockerimages/"
-	cp $ROOTDIR/$AWS_TARGET_PATH/*.tar packages/aws/dockerimages/
-fi
