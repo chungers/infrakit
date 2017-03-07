823fa2d8f9d636dc5c80a30b2f4896eea398b4e0
French Ben <FrenchBen@users.noreply.github.com>
2017-03-07 09:53:08 -0800

    Fix marketplace AMI bundle (#816)
    Signed-off-by: French Ben <frenchben@docker.com>

--- moby/get.sh
+++ moby/get.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-mkdir -p build/aws build/azure build/gcp src
+mkdir -p build/aws build/azure build/gcp src packages/aws/dockerimages/
 
 docker rm moby || true
 docker create --name moby mobylinux/media:${MOBY_IMG_COMMIT} true
 docker cp moby:/initrd.img src/initrd.img
 docker cp moby:/vmlinuz64 build/
 docker rm moby
+
+# Bundle the docker images if we're deploying to Marketplace
+if [ "$LOAD_IMAGES" == "true" ]; then
+	echo "+ Copying Docker images: from $ROOTDIR/$AWS_TARGET_PATH/*.tar to packages/aws/dockerimages/"		
+	cp $ROOTDIR/$AWS_TARGET_PATH/*.tar packages/aws/dockerimages/		
+	echo "+ Remove DDC and Cloud images from dockerimages"
+	rm -f packages/aws/dockerimages/ddc-init-aws.tar packages/aws/dockerimages/cloud-aws.tar || true
+fi
