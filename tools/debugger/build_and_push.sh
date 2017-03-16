2b677461e50dd9415d6f071476ad6a3ba7f0405a
French Ben <frenchben@docker.com>
2017-03-16 14:48:59 -0700

    Cleaned out unused dir
    Signed-off-by: French Ben <frenchben@docker.com>

--- tools/debugger/build_and_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VERSION=0.1
-docker build -t docker4x/debugger:$VERSION -f Dockerfile.debugger.aws .
-docker push docker4x/debugger:$VERSION
-
-docker build -t docker4x/node-debug:$VERSION -f Dockerfile.node-debug.aws .
-docker push docker4x/node-debug:$VERSION
