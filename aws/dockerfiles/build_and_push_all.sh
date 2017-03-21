c414ca0b7c6b1cdceafe3dce639b2a01773b264d
French Ben <FrenchBen@users.noreply.github.com>
2017-03-21 11:17:28 -0700

    Updated cloudstor to act as all other containers being pushed (#902)
    * Updated cloudstor to act as all other containers being pushed
    * Moved GCP and co to NAMESPACE
    * Removed un-needed ENV vars
    * Updated cloudstor build and push all
    
    Signed-off-by: French Ben <frenchben@docker.com>

--- aws/dockerfiles/build_and_push_all.sh
+++ aws/dockerfiles/build_and_push_all.sh
@@ -3,10 +3,10 @@ set -e
 
 # if there is an ENV with this name, use it, if not, default to these values.
 NAMESPACE="${NAMESPACE:-docker4x}"
-VERSION="${VERSION:-latest}"
+TAG_VERSION="${AWS_TAG_VERSION:-latest}"
 CURR_DIR=`pwd`
 ROOTDIR="${ROOTDIR:-$CURR_DIR}"
-DEFAULT_PATH="dist/aws/nightly/$VERSION"
+DEFAULT_PATH="dist/aws/nightly/$TAG_VERSION"
 AWS_TARGET_PATH="${AWS_TARGET_PATH:-$DEFAULT_PATH}"
 
 echo "+ Creating dist folder: $AWS_TARGET_PATH"
@@ -14,7 +14,7 @@ mkdir -p $ROOTDIR/$AWS_TARGET_PATH
 
 for IMAGE in shell init guide ddc-init cloud meta
 do
-	FINAL_IMAGE="${NAMESPACE}/${IMAGE}-aws:${VERSION}"
+	FINAL_IMAGE="${NAMESPACE}/${IMAGE}-aws:${TAG_VERSION}"
 	docker build --pull -t "${FINAL_IMAGE}" -f "Dockerfile.${IMAGE}" .
 	docker save "${FINAL_IMAGE}" > "${ROOTDIR}/${AWS_TARGET_PATH}/${IMAGE}-aws.tar"
 	if [ ${DOCKER_PUSH} -eq 1 ]; then
@@ -24,8 +24,8 @@ done
 
 # build and push cloudstor plugin
 tar zxvf cloudstor-rootfs.tar.gz -C files/
-docker plugin rm -f "${NAMESPACE}/cloudstor:${VERSION}" || true
-docker plugin create "${NAMESPACE}/cloudstor:${VERSION}" ./plugin
+docker plugin rm -f "${NAMESPACE}/cloudstor:${TAG_VERSION}" || true
+docker plugin create "${NAMESPACE}/cloudstor:${TAG_VERSION}" ./plugin
 if [ ${DOCKER_PUSH} -eq 1 ]; then
-	docker plugin push "${NAMESPACE}/cloudstor:${VERSION}"
+	docker plugin push "${NAMESPACE}/cloudstor:${TAG_VERSION}"
 fi
--- azure/dockerfiles/build_and_push_all.sh
+++ azure/dockerfiles/build_and_push_all.sh
@@ -3,11 +3,11 @@ set -e
 
 # if there is an ENV with this name, use it, if not, default to these values.
 NAMESPACE="${NAMESPACE:-docker4x}"
-VERSION="${VERSION:-latest}"
+TAG_VERSION="${AZURE_TAG_VERSION:-latest}"
 
 for IMAGE in init guide create-sp ddc-init cloud logger meta
 do
-	FINAL_IMAGE="${NAMESPACE}/${IMAGE}-azure:${VERSION}"
+	FINAL_IMAGE="${NAMESPACE}/${IMAGE}-azure:${TAG_VERSION}"
 	docker build --pull -t "${FINAL_IMAGE}" -f "Dockerfile.${IMAGE}" .
 	if [ ${DOCKER_PUSH} -eq 1 ]; then
 		docker push "${FINAL_IMAGE}"
@@ -16,7 +16,7 @@ done
 
 # Ensure that this image (meant to be interacted with manually) has :latest tag
 # as well as a specific one
-docker tag "${NAMESPACE}/create-sp-azure:${VERSION}" "${NAMESPACE}/create-sp-azure:latest"
+docker tag "${NAMESPACE}/create-sp-azure:${TAG_VERSION}" "${NAMESPACE}/create-sp-azure:latest"
 if [ ${DOCKER_PUSH}-eq 1 ]; then
 	docker push "${NAMESPACE}/create-sp-azure:latest"
 fi
@@ -24,8 +24,8 @@ fi
 
 # build and push cloudstor plugin
 tar zxvf cloudstor-rootfs.tar.gz -C files/
-docker plugin rm -f "${NAMESPACE}/cloudstor:${VERSION}" || true
-docker plugin create "${NAMESPACE}/cloudstor:${VERSION}" ./plugin
+docker plugin rm -f "${NAMESPACE}/cloudstor:${TAG_VERSION}" || true
+docker plugin create "${NAMESPACE}/cloudstor:${TAG_VERSION}" ./plugin
 if [ ${DOCKER_PUSH} -eq 1 ]; then
-	docker plugin push "${NAMESPACE}/cloudstor:${VERSION}"
+	docker plugin push "${NAMESPACE}/cloudstor:${TAG_VERSION}"
 fi
