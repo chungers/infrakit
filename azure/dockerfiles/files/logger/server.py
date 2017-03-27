203c273e8a6e10091a749e8cc61d8d9b4739446b
Deep Debroy <ddebroy@docker.com>
2017-03-27 00:22:13 -0700

    Stream Azure logs by mounting log files over SMB
    (cherry picked from commit 5a223c336ce9c7d60289355c5b879779aec876a5)

--- azure/dockerfiles/files/logger/server.py
+++ azure/dockerfiles/files/logger/server.py
@@ -1,37 +1,44 @@
 #!/usr/bin/env python
 
-## Tiny Syslog Server in Python.
+## Syslog Server that streams docker container logs over SMB to Azure file storage
 ##
-## This is a tiny syslog server that is able to receive UDP based syslog
-## entries on a specified port and save them to a file.
-## That's it... it does nothing else...
-## There are a few configuration parameters.
+## It receive UDP based syslog entries on a specified port and streams them 
+## to files on a SMB share hosted on Azure File Storage.
 
-LOG_PATH = '/log/'
+LOG_MNT_PATH = '/logmnt/'
+LOGS_SHARE_NAME = 'docker4azurelogs'
 HOST, PORT = "0.0.0.0", 514
 
-#
-# NO USER SERVICEABLE PARTS BELOW HERE...
-#
+CIFS_OPTION_VERSION  = "vers=2.1"
+CIFS_OPTION_FILE_MODE = "file_mode=0777"
+CIFS_OPTION_DIR_MODE = "dir_mode=0777"
+CIFS_OPTION_UID = "uid=0"
+CIFS_OPTION_GID = "gid=0"
 
+STORAGE_BASE = "core.windows.net"
+
+BUFFER_SZ_MAX = 1024*64    # 64 KB
+BUFFER_DURATION_MAX = 60   # 60 seconds
+CLEANUP_INTERVAL = 60*60*2 # 2 hrs
+IDLE_DURATION = 60*60*4    # 4 hrs
 
 # Azure log tactic:
-# 1. Use hostname to create folder in files
-# 2. Use container ID to create file
-# 3. Push data to file
+# 1. Mount file storage share in logging storage account over SMB
+# 2. Use container ID to create log files in mounted share
+# 3. Flush log files when buffer size or buffering duration exceeds thresholds
 
 import logging
 import SocketServer
 import os
 import argparse
 import sys
+import datetime
+import subprocess
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.storage import StorageManagementClient
 from azure.storage.file import (FileService, ContentSettings)
 from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, Regex
 from time import strftime
-
-logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filemode='a')
 
 class Parser(object):
 	def __init__(self):
@@ -67,17 +74,62 @@ class Parser(object):
 class AzureLog(object):
 	def __init__(self):
 		self.__key = self.get_storage_key()
-		self.file_service = self.create_file_service(self.__key)
-		
-	def write(self, container_log, message, level=logging.INFO):
-		global LOG_PATH
-		l = logging.getLogger(container_log)
-		if not len(l.handlers):
-			log_file = r"{0}{1}.log".format(LOG_PATH, container_log)
-			fileHandler = logging.FileHandler(log_file, mode='a')
-			l.addHandler(fileHandler)
-		l.info(message)
-		return l
+		self.file_service = self.create_file_service(self.__key, LOGS_SHARE_NAME)
+		self.account_name = os.environ['SWARM_LOGS_STORAGE_ACCOUNT']
+		self.mount_log_share(self.account_name, self.__key, LOGS_SHARE_NAME)
+
+		self.log_file_handles = {}
+		self.log_buffer_sizes = {}
+		self.log_flush_timestamps = {}
+		self.last_cleanup = datetime.datetime.utcnow()
+
+	def mount_log_share(self, account_name, account_key, share_name):
+		mount_uri = "//" + account_name + ".file." + STORAGE_BASE + "/" + share_name
+
+		mount_opts = [
+			CIFS_OPTION_VERSION,
+			"username=" + self.account_name,
+			"password=" + self.__key,
+			CIFS_OPTION_FILE_MODE,
+			CIFS_OPTION_DIR_MODE,
+			CIFS_OPTION_UID,
+			CIFS_OPTION_GID
+		]
+
+		if not os.path.exists(LOG_MNT_PATH):
+			os.makedirs(LOG_MNT_PATH)
+
+		mount_cmd = "mount -t cifs " + mount_uri + " " + LOG_MNT_PATH + " -o " + ",".join(mount_opts)
+		# raise on failure. If we can't mount the share, there's nothing we can do .. so die.
+		subprocess.check_call(mount_cmd, shell=True)  
+
+
+	def write(self, container_id, message):
+		# called in the context of a UDP recv call. Avoid calling blocking/remote APIs!
+		container_log_path = LOG_MNT_PATH + "/" + container_id
+		if not container_id in self.log_file_handles:
+			self.log_file_handles[container_id] = open(container_log_path, 'a')
+			self.log_buffer_sizes[container_id] = 0
+			self.log_flush_timestamps[container_id] = datetime.datetime.utcnow()
+
+		buffer_sz = self.log_buffer_sizes[container_id]
+		buffer_duration = datetime.datetime.utcnow() - self.log_flush_timestamps[container_id]
+		# reset the buffer and file handle to flush all contents at regular intervals
+		if ((buffer_sz != 0 and buffer_sz + len(message) > BUFFER_SZ_MAX) or 
+			(buffer_duration.seconds > BUFFER_DURATION_MAX)):
+			self.log_file_handles[container_id].close()
+			self.log_file_handles[container_id] = open(container_log_path, 'a')
+			buffer_sz = 0
+			self.log_flush_timestamps[container_id] = datetime.datetime.utcnow()
+
+		self.log_file_handles[container_id].write(message + "\n")
+		self.log_buffer_sizes[container_id] = buffer_sz + len(message)
+
+		#check if we need to garbage collect old container log entries
+		if (datetime.datetime.utcnow() - self.last_cleanup).seconds > CLEANUP_INTERVAL:
+			self.cleanup_file_entries(True)
+
+		return
 
 	def get_storage_key(self):
 		sub_id = os.environ['ACCOUNT_ID']
@@ -93,20 +145,25 @@ class AzureLog(object):
 		storage_keys = {v.key_name: v.value for v in storage_keys.keys}
 		return storage_keys['key1']
 
-	def create_file_service(self, storage_key):
+	def create_file_service(self, storage_key, share_name):
 		file_service = FileService(account_name=os.environ['SWARM_LOGS_STORAGE_ACCOUNT'], account_key=storage_key)
-		file_service.create_share(os.environ['SWARM_FILE_SHARE'])
-		return file_service
+		file_service.create_share(share_name)
+		return file_service     
 
-	def upload(self, container_log):
-		log_file = r"{0}{1}.log".format(LOG_PATH, container_log)
-		self.file_service.create_file_from_path(
-			os.environ['SWARM_FILE_SHARE'], # File share name
-			None, # We want to create this blob in the root directory, so we specify None for the directory_name
-			container_log, # file name on share
-			log_file, # the file to upload
-			content_settings=ContentSettings(content_type='text/plain')) # content type
-	
+	def cleanup_file_entries(self, check_time):
+		for container_id, log_file_handle in self.file_handles:
+			if check_time:
+				if (self.log_flush_timestamps[container_id] - datetime.datetime.utcnow() > IDLE_DURATION):
+					self.cleanup_file_entry(container_id)
+			else:
+				self.cleanup_file_entry(container_id)
+
+	def cleanup_file_entry(self, container_id):
+		close(self.log_file_handles[container_id])
+		del self.log_file_handles[container_id]
+		del self.log_buffer_sizes[container_id]
+		del self.log_flush_timestamps[container_id]   
+
 	# @TODO implement cleanup for container
 
 
@@ -118,11 +175,8 @@ class SyslogUDPHandler(SocketServer.BaseRequestHandler):
 		socket = self.request[1]
 		parser = Parser()
 		fields = parser.parse(str(data))
-		print( "%s : " % self.client_address[0], str(data))
-		container_log = "{0}-{1}".format(fields["containername"], fields["containerid"])
-		azure_log.write(container_log, fields["message"])
-		azure_log.upload(container_log)
-		# logging.info(fields)
+		container_id = "{0}-{1}".format(fields["containername"], fields["containerid"])
+		azure_log.write(container_id, fields["message"])
 
 if __name__ == "__main__":
 	try:
