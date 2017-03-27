124bb2245dec49914277b3dc28a4dc8d1a07a577
David Gageot <david@gageot.net>
2017-03-27 17:43:12 +0200

    No need to docker pull
    Images are written into Moby's base image and
    loaded at startup time.
    
    Fix #936
    
    Signed-off-by: David Gageot <david@gageot.net>

--- gcp/templates/startup.sh
+++ gcp/templates/startup.sh
@@ -10,27 +10,21 @@ export REGION="{{ REGION }}"
 export CHANNEL="beta"
 export NODE_TYPE="{{ type }}"
 
-shell_image="docker4x/shell-gcp:{{ VERSION }}"
-guide_image="docker4x/guide-gcp:{{ VERSION }}"
-lb_image="docker4x/lb-controller-gcp:{{ VERSION }}"
-infrakit_image="docker4x/infrakit-gcp:{{ VERSION }}"
+shell_image="docker4x/shell-gcp"
+guide_image="docker4x/guide-gcp"
+lb_image="docker4x/lb-controller-gcp"
+infrakit_image="docker4x/infrakit-gcp"
 
 docker_run='docker container run --label com.docker.editions.system --log-driver=json-file'
 docker_daemon="$docker_run --rm -d"
 docker_socket='-v /var/run/docker.sock:/var/run/docker.sock'
 docker_cli='-v /usr/bin/docker:/usr/bin/docker'
 
-function dockerPull {
-  for i in $(seq 1 60); do docker image pull $1 && break || sleep 1; done
-}
-
 {% if (type in ['leader']) %}
 echo Initialize Swarm
 
 docker node inspect self >/dev/null 2>&1 || docker swarm init --advertise-addr eth0:2377 --listen-addr eth0:2377
 docker node inspect self
-
-dockerPull ${guide_image}
 
 $docker_run --rm \
   -e NODE_TYPE \
@@ -57,13 +51,10 @@ $docker_run --rm \
 {% if (type in ['manager', 'leader']) %}
 echo Start infrakit
 
-dockerPull ${infrakit_image}
 $docker_daemon --name=infrakit $docker_socket $docker_cli $infrakit_image
 {% endif -%}
 
 echo Start sshd
-
-dockerPull ${shell_image}
 
 docker container inspect etc >/dev/null 2>&1 || $docker_run --name=etc -v /etc $shell_image true
 $docker_run --volumes-from=etc $shell_image /usr/bin/ssh-keygen.sh
@@ -94,7 +85,6 @@ $docker_daemon --name=shell \
 
 echo Start guide
 
-dockerPull ${guide_image}
 $docker_daemon --name=guide \
   -e NODE_TYPE \
   -e RUN_VACUUM="{{ properties['enableSystemPrune'] }}" \
@@ -109,7 +99,6 @@ $docker_daemon --name=guide \
 {% if (type in ['manager', 'leader']) %}
 echo Start Load Balancer Listener
 
-dockerPull ${lb_image}
 $docker_daemon --name=lbcontroller $docker_socket $lb_image run --log=5
 {% endif -%}
 
