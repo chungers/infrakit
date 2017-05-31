cfd8800222026216b7efab0397dffb314ee9e3d3
Deep Debroy <ddebroy@docker.com>
2017-05-31 10:59:54 -0700

    remove upgrade.sh from shell

--- azure/dockerfiles/walinuxagent/upgrade.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# wrapper around the python upgrade subscriptions
-[ $# -ne 1 ] && { echo "Usage: $0 new-version"; exit 1; }
-cont_id=$(docker ps -a | grep guide-azure | cut -d' ' -f1)
-echo executing upgrade on $cont_id
-docker exec -ti $cont_id /usr/bin/azupgrade.py $1
