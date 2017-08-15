411b0fdaf90085ba7850c2a1aad2f186b1f4587e
Corbin Coleman <corbincoleman@Dockers-MacBook-Pro-2.local>
2017-08-15 15:17:53 -0700

    Start service tests. Remove Makefile. Add custom function to lib.sh

---
+++ tests/cases/001_services/000_deploy_service/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=test
+
+clean_up() {
+    docker service remove "${NAME}"
+}
+
+trap clean_up EXIT
+
+docker service create --name "${NAME}" nginx
+NUM_REPLICAS=$(check_replicas "${NAME}" 1 5)
+
+[ "${NUM_REPLICAS}" = 1]
+docker service ls | assert_contains "${NAME}"
+
+exit 0
---
+++ tests/cases/001_services/001_service_replicated/test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+. "${RT_PROJECT_ROOT}/_lib/lib.sh"
+
+NAME=test
+
+clean_up() {
+    docker service remove "${NAME}"
+}
+
+trap clean_up EXIT
+
+docker service create --replicas 2 --name "${NAME}" nginx
+
+NUM_REPLICAS=$(check_replicas "${NAME}" 2 5)
+
+[ "${NUM_REPLICAS" = 2 ]
+docker service ps "${NAME}" | grep Running | assert_contains "swarm-worker000000"
+docker service ps "${NAME}" | grep Running | assert_contains "swarm-manager000000"
+
+exit 0
