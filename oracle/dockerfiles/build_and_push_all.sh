78d4831821e39f6db686462d2362dea676f432f9
French Ben <frenchben@docker.com>
2017-09-07 13:05:50 -0700

    Removed stale dtr folder
    Signed-off-by: French Ben <frenchben@docker.com>

--- oracle/dockerfiles/build_and_push_all.sh
+++ oracle/dockerfiles/build_and_push_all.sh
@@ -8,7 +8,7 @@ COMPOSE_VERSION="${COMPOSE_VERSION:-1.15.0}"
 
 CURR_DIR=`pwd`
 ROOT_DIR="${ROOT_DIR:-$CURR_DIR}"
-DEFAULT_PATH="dist/aws/nightly/$TAG_VERSION"
+DEFAULT_PATH="dist/oracle/nightly/$TAG_VERSION"
 ORACLE_TARGET_PATH="${ORACLE_TARGET_PATH:-$DEFAULT_PATH}"
 
 # Test all images built
@@ -21,7 +21,7 @@ function check_image () {
   TAG=${1#*:}
   NAMESPACE_IMAGE=${1%:*}
   IMAGE=${NAMESPACE_IMAGE#*/}
-  FOLDER=${IMAGE%*-aws} 
+  FOLDER=${IMAGE%*-oracle} 
   echo -e "+++ \033[1mTesting\033[0m \033[4m${1}\033[0m"
   docker container run --rm \
     -v ${CURR_DIR}/${FOLDER}/tests:/tests \
@@ -40,7 +40,7 @@ function docker_tag_exists() {
   TAG=${1#*:}
   NAMESPACE_IMAGE=${1%:*}
   IMAGE=${NAMESPACE_IMAGE#*/}
-  FOLDER=${IMAGE%*-aws}
+  FOLDER=${IMAGE%*-oracle}
   EXISTS=$(curl -f -slSL https://hub.docker.com/v2/repositories/${NAMESPACE_IMAGE}/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"${TAG}\"] | any")
   #test $EXISTS = true
   true
@@ -56,7 +56,7 @@ fi
 
 for IMAGE in shell init guide ddc-init cloud meta
 do
-  FINAL_IMAGE="${NAMESPACE}/${IMAGE}-aws:${TAG_VERSION}"
+  FINAL_IMAGE="${NAMESPACE}/${IMAGE}-oracle:${TAG_VERSION}"
   echo -e "++ \033[1mBuilding image:\033[0m ${FINAL_IMAGE}"
   BUILD_ARGS=""
   if [ "$IMAGE" = "shell" ]; then
@@ -73,11 +73,11 @@ do
   fi
 done
 
-# build and push cloudstor plugin
-tar zxf cloudstor-rootfs.tar.gz
-docker plugin rm -f "${NAMESPACE}/cloudstor:${TAG_VERSION}" || true
-docker plugin create "${NAMESPACE}/cloudstor:${TAG_VERSION}" ./plugin
-rm -rf ./plugin
-if [ "${DOCKER_PUSH}" = true ]; then
-  docker plugin push "${NAMESPACE}/cloudstor:${TAG_VERSION}"
-fi
+# # build and push cloudstor plugin
+# tar zxf cloudstor-rootfs.tar.gz
+# docker plugin rm -f "${NAMESPACE}/cloudstor:${TAG_VERSION}" || true
+# docker plugin create "${NAMESPACE}/cloudstor:${TAG_VERSION}" ./plugin
+# rm -rf ./plugin
+# if [ "${DOCKER_PUSH}" = true ]; then
+#   docker plugin push "${NAMESPACE}/cloudstor:${TAG_VERSION}"
+# fi
